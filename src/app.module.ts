bb1915c2f6f24e397485de1ce26960be570b4445
santisiele <santi.siele@gmail.com>
2026-06-11 15:25:04 -0300

    feat: set up NestJS modules and Prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { CompaniesModule } from './companies/companies.module';
+import { ContactsModule } from './contacts/contacts.module';
+import { TasksModule } from './tasks/tasks.module';
+import { TaskActivitiesModule } from './task-activities/task-activities.module';
+import { TaskAssignmentsModule } from './task-assignments/task-assignments.module';
 
 @Module({
-  imports: [],
+  imports: [
+    PrismaModule,
+    AuthModule,
+    CompaniesModule,
+    ContactsModule,
+    TasksModule,
+    TaskActivitiesModule,
+    TaskAssignmentsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
